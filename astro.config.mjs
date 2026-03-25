041594d710e0cf63ba78de8324576def966e9eb1
Agust18 <agustincamera16@hotmail.com>
2026-03-25 14:37:09 -0300

    optimizando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ]
+  },
   vite: {
     plugins: [tailwindcss()]
   }
